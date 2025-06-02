672d65e88ea5838735598d6ca1fbb1af02a45fdc
Bhavya-Makadia <bhavyamakadia11@gmail.com>
2025-06-02 10:19:12 +0530

    [type identifier]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <algorithm>
 using namespace std;
 
 int main()
@@ -15,18 +16,42 @@ int main()
     cout << "$ "; 
     getline(cin, input);
 
-
-    if(input == "exit 0"){
-      return 0;
-    }
-
     istringstream iss(input);
     string command;
     iss >> command;
-    if( command == "echo"){
+
+    string shell_builtins[] = {
+    " alias", " bg", " bind", " break", " builtin", " caller", " cd", " command",
+    " compgen", " complete", " compopt", " continue", " declare", " dirs",
+    " disown", " echo", " enable", " eval", " exec", " exit", " export", " false",
+    " fc", " fg", " getopts", " hash", " help", " history", " jobs", " kill", " let",
+    " local", " logout", " mapfile", " popd", " printf", " pushd", " pwd", " read",
+    " readarray", " readonly", " return", " set", " shift", " shopt", " source",
+    " suspend", " test", " times", " trap", " true", " type", " typeset", " ulimit",
+    " umask", " unalias", " unset", " wait"
+    };
+
+    if(input == "exit 0"){
+      return 0;
+    } else if( command == "echo"){
       string output;
       getline(iss, output);
       cout << output.substr(1) << endl;
+    } else if(  command == "type" ) {
+      string output;
+      getline(iss, output);
+      bool found = false;
+      size_t num_builtins = sizeof(shell_builtins) / sizeof(shell_builtins[0]);
+      for ( size_t i = 0; i < num_builtins; i++){
+        if ( output == shell_builtins[i]){
+          cout << output << " is a shell builtin"<< endl;
+          found = true;
+          break;
+        } 
+      }
+      if (!found) {
+        cout << output << ": not found" << endl;
+      }
     } else {
       cout << input << ": command not found" << endl;
     }
