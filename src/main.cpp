f31b7e47cd2c46f040a92d8051d460b6e16c02fe
Bhavya-Makadia <bhavyamakadia11@gmail.com>
2025-07-19 09:45:35 +0530

    [Updated concise code]

--- src/main.cpp
+++ src/main.cpp
@@ -1,109 +1,66 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-#include <algorithm>
-#include <vector>
-#include <filesystem>
-#include <cstdlib>
-#include <fstream>
+#include <map>
 #include <unistd.h>
-using namespace std;
+#include <cstring>
 
-string trimInitialWhitespace(string str)
-{
-  str.erase(0, str.find_first_not_of(" \t\n\r"));
-  return str;
+std::string find_executable_path(std::string command);
+
+
+int main() {
+  // Flush after every std::cout / std:cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  std::string input;
+
+  std::map<std::string, bool> commands = {
+    {"echo", true},
+    {"exit", true},
+    {"type", true}
+  };
+
+  std::cout << "$ ";
+
+  while (std::getline(std::cin, input)) {
+    if (input == "exit 0") {
+      exit(0);
+    } else if (input.compare(0, 4, "echo") == 0) {
+      std::cout << input.substr(5) << std::endl;
+    } else if (input.compare(0, 4, "type") == 0) {
+      std::string command = input.substr(5);
+      std::string executable_path = find_executable_path(command);
+      
+      if (commands.find(command) != commands.end()) {
+        std::cout << command << " is a shell builtin" << std::endl;
+      } else if(executable_path != "") {
+        std::cout << command << " is " << executable_path << std::endl;
+      } else {
+        std::cout << command << ": not found" << std::endl;
+      }
+    } else if (find_executable_path(input.substr(0, input.find(" "))) != "") {
+      std::system(input.c_str());
+    } else {
+      std::cout << input << ": command not found" << std::endl;
+    }
+
+    std::cout << "$ ";
+  }
 }
 
-int main()
-{
-  // Flush after every std::cout / std:cerr
-  cout << std::unitbuf;
-  cerr << std::unitbuf;
-  string input;
-
-  while (true)
-  {
-    cout << "$ ";
-    getline(cin, input);
-
-    istringstream iss(input);
-    string command;
-    iss >> command;
-
-    string shell_builtins[] = {
-        "alias", "bg", "bind", "break", "builtin", "caller", "cd", "command",
-        "compgen", "complete", "compopt", "continue", "declare", "dirs",
-        "disown", "echo", "enable", "eval", "exec", "exit", "export", "false",
-        "fc", "fg", "getopts", "hash", "help", "history", "jobs", "kill", "let",
-        "local", "logout", "mapfile", "popd", "printf", "pushd", "pwd", "read",
-        "readarray", "readonly", "return", "set", "shift", "shopt", "source",
-        "suspend", "test", "times", "trap", "true", "type", "typeset", "ulimit",
-        "umask", " nalias", "unset", "wait"};
-
-    if (input == "exit 0")
-    {
-      return 0;
+std::string find_executable_path(std::string command) {
+  char *path = getenv("PATH");
+  char *path_copy = strdup(path);
+  char *token = strtok(path_copy, ":");
+  while (token != NULL) {
+    std::string curPath = token;
+    curPath += "/" + command;
+    if (access(curPath.c_str(), X_OK) == 0) {
+      free(path_copy);
+      return curPath;
     }
-    else if (command == "echo")
-    {
-      string output;
-      getline(iss, output);
-      cout << output.substr(1) << endl;
-    }
-    else if (command == "type")
-    {
-      string output;
-      getline(iss, output);
-
-      output = trimInitialWhitespace(output);
-      bool is_builtin = false;
-      bool is_Executable = false;
-
-      size_t num_builtins = sizeof(shell_builtins) / sizeof(shell_builtins[0]);
-      for (size_t i = 0; i < num_builtins; i++)
-      {
-        if (output == shell_builtins[i])
-        {
-          cout << output << " is a shell builtin" << endl;
-          is_builtin = true;
-          break;
-        }
-      }
-      if (!is_builtin)
-      {
-        char *pathEnv = getenv("PATH");
-        if (pathEnv == nullptr)
-        {
-          cout << output << ": not found" << endl;
-          continue;
-        }
-
-        string path_str(pathEnv);
-        istringstream ss(path_str);
-        string dir;
-
-        while (getline(ss, dir, ':'))
-        {
-          string full_path = dir + "/" + output;
-
-          if (access(full_path.c_str(), X_OK) == 0)
-          {
-            cout << output << " is " << full_path << endl;
-            is_Executable = true;
-            break;
-          }
-        }
-      }
-
-      if (!is_builtin && !is_Executable)
-      {
-        cout << output << ": not found" << endl;
-      }
-    }
-    else
-    {
-      cout << input << ": command not found" << endl;
-    }
+    token = strtok(NULL, ":");
   }
+  free(path_copy);
+  return "";
 }
