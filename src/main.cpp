ee77a87487adb724f54b5c08c98306a3e1649121
Bhavya-Makadia <bhavyamakadia11@gmail.com>
2025-08-15 12:36:50 +0530

    [write history to file]

--- src/main.cpp
+++ src/main.cpp
@@ -274,7 +274,23 @@ int main()
       }
     } else if (input.compare(0, 7, "history") == 0){
       size_t space = input.find(' ');
-      if (space != string::npos && input.substr(space + 1, 2) == "-r") {
+      if (space != string::npos && input.substr(space + 1, 2) == "-w") {
+        size_t file_space = input.find(' ', space and + 1);
+        if (file_space != string::npos) {
+            string history_file = input.substr(file_space + 1);
+            ofstream file(history_file);
+            if (file.is_open()) {
+                for (const auto& command : commandHistory) {
+                    file << command << endl;
+                }
+                file.close();
+            } else {
+                cout << "Unable to open history file" << endl;
+            }
+        } else {
+            cout << "history: -w requires a file name" << endl;
+        }
+    } else if (space != string::npos && input.substr(space + 1, 2) == "-r") {
         size_t file_space = input.find(' ', space + 1);
         if (file_space != string::npos) {
             string history_file = input.substr(file_space + 1);
